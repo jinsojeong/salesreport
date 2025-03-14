d938e32c5720eec85fd620d340cc1869601b738c
jinsojeong <qksk5168@gmail.com>
2025-03-14 16:02:31 +0900

    Main.view.xml 에 List로 sales 하나씩 띄우기

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,7 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
-  "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], (BaseController) => {
+    "use strict";
 
-  return BaseController.extend("sync.c23.salesreport.controller.App", {
-      onInit() {
-      }
-  });
+    return BaseController.extend("sync.c23.salesreport.controller.App", {
+        onInit() {},
+    });
 });
